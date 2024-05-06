845932128161b59724b8f0749df4fc2c06f82fa9
Herbert Marshall <herbert@herbmarshall.com>
2024-05-06 08:27:43 -0400

    UTIL-340 Add copyright

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# This file is part of herbmarshall.com: project.pom  ( hereinafter "project.pom" ).
+# project.pom is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+# project.pom is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with project.pom. If not, see <https://www.gnu.org/licenses/>.
+#
+
 set -e
 
 # Using non-cdlocal because may be ran by jenkins
